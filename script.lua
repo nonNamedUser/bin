8180b4f11f0e8942b30ac2339ae4ae5dee103b35
caspergtag <109670694+nonNamedUser@users.noreply.github.com>
2024-06-11 20:08:12 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -4,7 +4,5 @@ local text=get("welcome")
 
 button.on_click(function()
     local table = {"Hello", "Hi", "Cześć", "Halo", "Gutten Tag", "Bonjour"}
-
-    local w=table[math.random(#table)]
-    text.set_content(w)
+    text.set_content("Hi")
 end)
